457dcdd3b5517a98449a4c78d07cd5f1c3785c7b
gabriellagmoreira <gabigmoreira@gmail.com>
2024-11-11 19:15:16 -0300

    funçoes

--- src/modulos/operacoes.py
+++ src/modulos/operacoes.py
@@ -1,17 +1,26 @@
+import math  # Import necessário para usar a função sqrt()
 
-def soma(x,y):
-    result = x + y
-    return result
+def soma(x, y):
+    return x + y
 
-def subtração(x,y):
-    result = x - y
-    return result
+def subtracao(x, y):
+    return x - y
 
-def multiplicação (x,y):
-    result = x * y
-    return result
+def multiplicacao(x, y):
+    return x * y
 
 def divisao(x, y):
     if y == 0:
         return "Erro: Divisão por zero não é permitida."
     return x / y
+
+def potencia(x, y):
+    return x ** y
+
+def raiz_quadrada(x):
+    if x < 0:
+        return "Erro: Raiz quadrada de número negativo não é permitida."
+    return math.sqrt(x)
+
+def modulo(x, y):
+    return x % y
